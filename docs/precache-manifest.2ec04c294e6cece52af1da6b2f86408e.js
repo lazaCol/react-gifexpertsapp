fa12946b5ac8cfa76d87e109a912d08442e2de9b
Luis Miguel Aza Caicedo <laza@Colsof.com.co>
2020-10-06 09:13:13 -0500

    actualizacion docs para githubpage

---
+++ docs/precache-manifest.2ec04c294e6cece52af1da6b2f86408e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa51fb7671428b8712c32c80a02f26eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ceb33c9c70c7545afdb0",
+    "url": "/static/css/main.d49d2e28.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ceb33c9c70c7545afdb0",
+    "url": "/static/js/main.d8274ecb.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
